dce40f83a4d3add82c23277ab54c342944b70cdc
Eugene Vakerin <utyfdfrthby08@gmail.com>
2021-05-13 22:02:40 +0300

    examples with different ui elements (iframes)

---
+++ cypress/integration/Examples/2. Iframes.js
@@ -0,0 +1,20 @@
+describe('IFrames', function () {
+    it('should test iframe 1', function () {
+        /*
+        * 1. Нам нужен плагин -> npm install -D cypress-iframe
+        * 2. Добавить импорт в commands.js -> import 'cypress-iframe';
+        * 3. У нас есть команда cy.iframe()
+        * 4. Находим фрейм , потом после .then находим внутри него нужный элемент
+        * 5. Заворачиваем результат jquery функции в cy.wrap() и кликаем уже через cypress
+        */
+        cy.visit('https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe?retiredLocale=uk');
+        cy.iframe('.interactive').then((iframe) => {
+            let element = iframe.contents().find("#css");
+            cy.wrap(element).click();
+        })
+    });
+
+    it.only('should test iframe 2', function () {
+
+    });
+});
