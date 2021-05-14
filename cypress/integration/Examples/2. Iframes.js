392b07116bfa14d4cfde513ba20945b3284b3b66
Eugene Vakerin <utyfdfrthby08@gmail.com>
2021-05-14 21:50:55 +0300

    all examples with iframes

--- cypress/integration/Examples/2. Iframes.js
+++ cypress/integration/Examples/2. Iframes.js
@@ -14,7 +14,16 @@ describe('IFrames', function () {
         })
     });
 
-    it.only('should test iframe 2', function () {
+    it('should test iframe 2', function () {
+        cy.visit('https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe?retiredLocale=uk');
+        cy.get('.interactive').its('0.contentDocument.body').then(cy.wrap).find('#css').click();
+    });
 
+    it.only('should test iframe3', function () {
+        cy.visit('https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe?retiredLocale=uk');
+        cy.iframe('.interactive').then((iframe) => {
+            cy.wrap(iframe).as('iframe');
+        })
+        cy.get('@iframe').find('#css').click();
     });
 });
